edff247e2eefa72c8d3c7beb32c5628ed3469a62
pmy0792 <pmy0792@naver.com>
2022-05-05 17:56:06 +0300

    practice view

--- personality-test/firstapp/views.py
+++ personality-test/firstapp/views.py
@@ -1,11 +1,36 @@
+from lib2to3.refactor import MultiprocessingUnsupported
+from pydoc_data.topics import topics
 from django.http import HttpResponse
 from django.shortcuts import render,HttpResponse
 
-# Create your views here.
+menus=[
+    {'id':1, 'title':'start menu'},
+    {'id':2, 'title': 'share'}
+]
+
+
 
 def index(request):
+    global menus
+    ul=''
+    for menu in menus:
+        ul+=f'<li><a href="/read/{menu["id"]}>{menu["title"]} </li>'
+        print(ul)
+        
+    return HttpResponse(f'''
+        <html>
+        <body>
+        <ul>
+        {ul}
+        </ul>
+        </body>
+        </html>
+                        ''')
+    
+'''
+def index(request):
     return HttpResponse("Welcome") #response by http to client
-
+'''
 
 def result(request, type):
     return HttpResponse("Result: "+type)
