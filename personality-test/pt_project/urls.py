6f47e15f92e1290a04b2c4ea217a02fdaa59e317
pmy0792 <pmy0792@naver.com>
2022-05-20 04:19:25 +0300

    setting for image upload

--- personality-test/pt_project/urls.py
+++ personality-test/pt_project/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf.urls.static import static
-from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # routing information
 
@@ -24,4 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls), #default page for django administration
     path('',include('firstapp.urls')) #위임하려는 app의 이름을 두번째 인자에
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
