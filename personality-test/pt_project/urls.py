438552a14a8cd8ad4c6664c381b3eb46ecff4975
pmy0792 <pmy0792@naver.com>
2022-05-22 22:29:59 +0300

    Media file re_path

--- personality-test/pt_project/urls.py
+++ personality-test/pt_project/urls.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 #from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
 # routing information
 
 urlpatterns = [
     path('admin/', admin.site.urls), #default page for django administration
     path('',include('firstapp.urls')), #위임하려는 app의 이름을 두번째 인자에
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 ]
 #urlpatterns += staticfiles_urlpatterns()
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
