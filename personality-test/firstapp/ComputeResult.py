c4c041903d2ad96d8e9f592a2bf2fcc3bdd93bba
pmy0792 <pmy0792@naver.com>
2022-05-22 03:04:01 +0300

    Script for calculate the result

---
+++ personality-test/firstapp/ComputeResult.py
@@ -0,0 +1,45 @@
+import sqlite3
+from .models import *
+
+mbti_and_result={
+    'ESFJ':'Coffee Shop Barista',
+    'ENTJ':'Cinema Staff',
+    'ISTJ':'General Assistant',
+    'ESFP':'LUSH Staff',
+    'ENFP':'Amusement Park Staff',
+    'ISTP':'Convenience Store Staff',
+    'INFJ':'Office Assistant',
+    'ISFJ':'Wedding Guest',
+    'ISFP':'Bakery Staff',
+    'INTP':'Blogger',
+    'INTJ':'Private Tutor',
+    'ESTJ':'Exam Supervisor',
+    'ESTP':'Folk Village Staff',
+    'INFP':'Pet Sitter',
+    'ENTP':'Academy Instructor',
+    'ENFJ':'Kids Cafe Staff'  
+}
+
+def calc_result(user):
+    points={'EI':0, 'NS':0,'TF':0,'JP':0}
+    vote=Voting.objects.filter(user_ip__user_ip=user).values('question','choice')
+    #choice=list(vote.values('choice'))[-12:] # the latest 12 choices of the user
+    q_and_c=vote.order_by('-id')[:12]
+    for qc in q_and_c:
+        q=Question.objects.get(pk=qc['question'])
+        c=Choice.objects.get(pk=qc['choice'])
+        q_type=q.question_type
+        c_point=c.choice_point
+        
+        points[q_type]+=c_point
+        
+    mbti_type=""
+    mbti_type+=("E" if points['EI']>=2 else "I")
+    mbti_type+=("N" if points['NS']>=2 else "S")
+    mbti_type+=("T" if points['TF']>=2 else "F")
+    
+    for key in points:
+        mbti_type += (key[0] if points[key]>=2 else key[1])
+        
+    result_type=mbti_and_result(mbti_type)
+    return result_type
